690a313d67fcb5d7b904bd0636e61adc1d097dfe
Tyler <tylergwlum@gmail.com>
2018-11-23 17:52:45 -0800

    Put perceptron code in main, able to be imported

--- perceptron.py
+++ perceptron.py
@@ -30,44 +30,44 @@ def plot_history(h, y_variable):
     plt.legend([y_variable])
     plt.show()
 
-## Make random 2D arrays
-n_pts = 500
-np.random.seed(0)
-standard_deviation = 2
-Xa = np.array([np.random.normal(13, standard_deviation, n_pts), np.random.normal(12, standard_deviation, n_pts)]).T
-Xb = np.array([np.random.normal(8, standard_deviation, n_pts), np.random.normal(6, standard_deviation, n_pts)]).T
-
-X = np.vstack((Xa, Xb))
-Y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
-
-## Scatter plot
-plt.scatter(X[:n_pts, 0], X[:n_pts, 1])
-plt.scatter(X[n_pts:, 0], X[n_pts:, 1])
-# plt.show()
-
-## Create perceptron
-model = Sequential()
-model.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
-adam = Adam(lr = 0.1)
-model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
-h = model.fit(x=X, y=Y, verbose=1, batch_size=50, epochs=300, shuffle='true')
-
-## Plot accuracy
-# plot_history(h, 'acc')
-
-## Plot loss
-# plot_history(h, 'loss')
-
-## Plot decision boundary and training data points
-plot_decision_boundary(X, Y, model)
-plt.scatter(X[:n_pts, 0], X[:n_pts, 1])
-plt.scatter(X[n_pts:, 0], X[n_pts:, 1])
-
-## Get predict new data
-x, y = 7.5, 5
-point = np.array([[x, y]])
-print(point)
-prediction = model.predict(point)
-plt.plot([x], [y], marker='o', markersize=10, color='b')
-print("Prediction is {0}".format(prediction))
-plt.show()
+if __name__ == '__main__':
+    ## Make random 2D arrays
+    n_pts = 500
+    np.random.seed(0)
+    standard_deviation = 2
+    Xa = np.array([np.random.normal(13, standard_deviation, n_pts), np.random.normal(12, standard_deviation, n_pts)]).T
+    Xb = np.array([np.random.normal(8, standard_deviation, n_pts), np.random.normal(6, standard_deviation, n_pts)]).T
+    
+    X = np.vstack((Xa, Xb))
+    Y = np.matrix(np.append(np.zeros(n_pts), np.ones(n_pts))).T
+    
+    ## Scatter plot
+    plt.scatter(X[:n_pts, 0], X[:n_pts, 1])
+    plt.scatter(X[n_pts:, 0], X[n_pts:, 1])
+    # plt.show()
+    
+    ## Create perceptron
+    model = Sequential()
+    model.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
+    adam = Adam(lr = 0.1)
+    model.compile(adam, loss='binary_crossentropy', metrics=['accuracy'])
+    h = model.fit(x=X, y=Y, verbose=1, batch_size=50, epochs=300, shuffle='true')
+    
+    ## Plot accuracy
+    # plot_history(h, 'acc')
+    
+    ## Plot loss
+    # plot_history(h, 'loss')
+    
+    ## Plot decision boundary and training data points
+    plot_decision_boundary(X, Y, model)
+    plt.scatter(X[:n_pts, 0], X[:n_pts, 1])
+    plt.scatter(X[n_pts:, 0], X[n_pts:, 1])
+    
+    ## Get predict new data
+    x, y = 7.5, 5
+    point = np.array([[x, y]])
+    prediction = model.predict(point)
+    plt.plot([x], [y], marker='o', markersize=10, color='b')
+    print("Point ({0}, {1}): Prediction is {2}".format(x, y, prediction))
+    plt.show()
